0a5188cb906125dbae82d67efaa4b86f849b411e
VindexTOS <giorgikutateladze1998@gmail.com>
2023-10-27 23:55:39 +0400

    vector divs

---
+++ src/components/content/indext.tsx
@@ -0,0 +1,18 @@
+import { ReactElement } from 'react'
+
+interface contentProps {
+  style?: string
+  border: string
+  children: ReactElement
+}
+
+export default function Content({ style, children, border }: contentProps) {
+  return (
+    <div className="relatie ">
+      {/* <div className={`${border}`}></div> */}
+
+      <div className={`${style}`}> {children}</div>
+      {border && <div className={`${border}`}></div>}
+    </div>
+  )
+}
--- src/pages/root/index.tsx
+++ src/pages/root/index.tsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Layout from '../layout'
+import Content from '../../components/content/indext'
 const Root = () => {
   return (
     <Layout>
-      <Outlet />
+      {/* <Outlet /> */}
+
+      <div className="flex flex-col  relative    w-[100%] h-[100%]">
+        <Content style="vector1" border="vector1-border">
+          <div className="flex flex-col items-center justify-center h-[90%]  ">
+            vector1
+          </div>
+        </Content>
+        <Content style="vector2" border="vector2-border  ">
+          <div>VEctor2</div>
+        </Content>
+        <Content style="vector2" border="vector2-border  ">
+          <div>VEctor2</div>
+        </Content>
+        <Content style="vector3" border=" vector3-border ">
+          <div>VEctor3</div>
+        </Content>
+        <Content style="vector4" border="vector4-border   ">
+          <div>VEctor4</div>
+        </Content>
+        <Content style="vector5" border="vector5-border ">
+          <div>VEctor5</div>
+        </Content>
+      </div>
     </Layout>
   )
 }
