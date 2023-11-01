548840620a37b23418ac165a13f837ad1b4a0090
John Jang <jscolbe9675@gmail.com>
2023-11-01 11:29:02 -0400

    added Admin Dashboard, Producer Dashboard, and Evaluator Dashboard pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { GeneralContextProvider } from "./contexts/GeneralContext";
 import Home from "./pages/home";
 import Root from "./pages/root";
+import AdminDashboard from "./pages/admin/admin-dashboard";
+import ProducerDashboard from "./pages/producer/producer-dashboard/ProducerDashboard";
+import EvaluatorDashboard from "./pages/evaluator/evaluator-dashboard/EvaluatorDashboard";
 
 export default function App() {
   const router = createBrowserRouter([
@@ -13,7 +16,12 @@ export default function App() {
         </GeneralContextProvider>
       ),
       // errorElement: <NotFound />,
-      children: [{ index: true, element: <Home /> }],
+      children: [
+        { index: true, element: <Home /> },
+        {path: '/admin', element: <AdminDashboard/>},
+        {path: '/producer', element: <ProducerDashboard/>},
+        {path: '/evaluator', element: <EvaluatorDashboard/>},
+      ],
     },
   ]);
 
