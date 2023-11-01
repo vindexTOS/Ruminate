5b61dee24a43fb4af07f970f2f513fc059cca799
John Jang <jscolbe9675@gmail.com>
2023-11-01 11:45:49 -0400

    draft version of nav buttons added

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,10 +1,24 @@
  
+import { Link } from "react-router-dom";
 import brandLogo from "../../assets/images/brand-logo.svg";
 
 export default function NavBar() {
   return (
-    <nav className="bg-brand-green px-[72px] py-[30px]">
+    <nav className="bg-brand-green px-[72px] py-[30px] flex justify-between">
+      <div>
       <img alt="Feedback logo" src={brandLogo} />
+      </div>
+      <div className="flex gap-5">
+        <div>
+        <Link to='/admin' className="text-white">Admin</Link>
+        </div>
+        <div>
+        <Link to='/producer' className="text-white">Producer</Link>
+        </div>
+        <div>
+        <Link to='/evaluator' className="text-white">Evaluator</Link>
+        </div>
+      </div>
     </nav>);
  
 }
